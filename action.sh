fac1402cae444ed4734309e0f97e5bfa89ad3139
Sander Jokiel Høyvik <2852267+TinyAngryKitten@users.noreply.github.com>
2020-07-04 20:23:31 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,4 +2,4 @@ export CLI_TOKEN=$1
 export APPID=$2
 export GROUPS=$3
 
-firebase appdistribution:distribute app/build/outputs/apk/release/app-release-unsigned.apk --app $APPID --token $CLI_TOKEN --groups $GROUPS
+firebase appdistribution:distribute app-release-unsigned.apk --app $APPID --token $CLI_TOKEN --groups $GROUPS
