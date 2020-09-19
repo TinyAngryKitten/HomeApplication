3bc721e2ae24dc74a347c15e131a125f524d5e84
sander høyvik <sander.hoyvik@gmail.com>
2020-09-19 20:30:21 +0200

    vibrate on tv control buttons

--- app/src/main/java/tiny/angry/kitten/homeapplication/views/TVControlView.kt
+++ app/src/main/java/tiny/angry/kitten/homeapplication/views/TVControlView.kt
@@ -3,21 +3,25 @@ package tiny.angry.kitten.homeapplication.views
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
+import android.content.Context.VIBRATOR_SERVICE
+import android.os.Build
 import android.os.Bundle
-import android.util.AttributeSet
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.Gravity
-import android.view.View
 import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.tv_control_view.*
 import kotlinx.android.synthetic.main.tv_control_view.view.*
+import org.koin.core.KoinComponent
 import tiny.angry.kitten.homeapplication.R
 import tiny.angry.kitten.homeapplication.invocation.harmony.HarmonyInvoker
 import tiny.angry.kitten.topics.harmony.HarmonyHubActions
 import tiny.angry.kitten.topics.harmony.HarmonyHubDevices
 
-class TvControlView(context: Context) : LinearLayout(context) {
+
+class TvControlView(context: Context) : LinearLayout(context), KoinComponent {
     val harmonyController = HarmonyInvoker()
 
     init {
@@ -25,41 +29,59 @@ class TvControlView(context: Context) : LinearLayout(context) {
 
         tvPowerButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.TV,HarmonyHubActions.PowerToggle)
+            vibrate()
         }
         appleTvPower.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.APPLE_TV,HarmonyHubActions.Menu)
+            vibrate()
         }
 
         upButton.setOnClickListener{
             harmonyController.invokeAction(HarmonyHubDevices.APPLE_TV,HarmonyHubActions.DirectionUp)
+            vibrate()
         }
         downButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.APPLE_TV,HarmonyHubActions.DirectionDown)
+            vibrate()
         }
         rightButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.APPLE_TV,HarmonyHubActions.DirectionRight)
+            vibrate()
         }
         leftButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.APPLE_TV,HarmonyHubActions.DirectionLeft)
+            vibrate()
         }
         OKButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.APPLE_TV,HarmonyHubActions.Select)
+            vibrate()
         }
         backButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.APPLE_TV,HarmonyHubActions.Back)
+            vibrate()
         }
         appletvButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.TV,HarmonyHubActions.InputHdmi3)
+            vibrate()
         }
         playstationButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.TV,HarmonyHubActions.InputHdmi1)
+            vibrate()
         }
         plusButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.TV,HarmonyHubActions.VolumeUp)
+            vibrate()
         }
         minusButton.setOnClickListener {
             harmonyController.invokeAction(HarmonyHubDevices.TV,HarmonyHubActions.VolumeDown)
+            vibrate()
         }
+    }
+
+    fun vibrate() {
+        val vibrator =
+            context.getSystemService(VIBRATOR_SERVICE) as Vibrator?
+        vibrator?.vibrate(VibrationEffect.createOneShot(100L,10))
     }
 }
 
