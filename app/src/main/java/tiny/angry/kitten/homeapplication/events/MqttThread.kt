745d0f9cce229bf0fabd849c14642008f8738663
sander høyvik <sander.hoyvik@gmail.com>
2020-09-26 15:54:42 +0200

    crashlytics

--- app/src/main/java/tiny/angry/kitten/homeapplication/events/MqttThread.kt
+++ app/src/main/java/tiny/angry/kitten/homeapplication/events/MqttThread.kt
@@ -4,11 +4,10 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import io.vertx.mqtt.MqttClient
 import io.vertx.mqtt.messages.MqttPublishMessage
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.core.qualifier.named
@@ -18,25 +17,35 @@ class MqttThread : Service(), KoinComponent{
     val host : String by inject(named("host"))
     val port : Int by inject(named("port"))
     val handlers: List<MqttEventHandler> by inject(named("messageHandlers"))
-    var stayConnected = true
+    val crashlytics : FirebaseCrashlytics by inject()
 
+    var stayConnected = true
+    var mqttJob : Job = Job().apply { cancel() }
 
     fun connect() {
-        GlobalScope.launch {
-            while(stayConnected) {
+        client.exceptionHandler(::handleException)
+        
+        mqttJob = MainScope().launch {
+            withContext(Dispatchers.IO) {
+                while (stayConnected) {
 
-                if (!client.isConnected) {
-                    client.connect(port, host) { connectionMessage ->
-                        Log.i("MQTT", "Connected to broker")
+                    if (!client.isConnected) {
+                        client.connect(port, host) { connectionMessage ->
+                            Log.i("MQTT", "Connected to broker")
 
-                        client.publishHandler(::handleMessages)
-                        subscribeToHandlerTopics()
+                            client.publishHandler(::handleMessages)
+                            subscribeToHandlerTopics()
+                        }
                     }
-                }
 
-                delay(10000)
+                    delay(10000)
+                }
             }
         }
+    }
+
+    fun handleException(exception : Throwable) {
+        crashlytics.recordException(exception)
     }
 
     fun handleMessages(message: MqttPublishMessage) =
